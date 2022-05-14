4d499b50b06e2f9d0bf13c5dfcb59dcea9556a8c
hugo fortunier <hfortunier@gmail.com>
2022-05-14 15:32:40 +0200

    wip

--- app/controllers/user_recipes_controller.rb
+++ app/controllers/user_recipes_controller.rb
@@ -1,9 +1,18 @@
 class UserRecipesController < ApplicationController
+  before_action :find_user_recipe, only: [:show]
   def index
     @user_recipes = UserRecipe.where(user: current_user).includes([:recipe])
   end
 
   def show
-    @user_recipe = ScrappMarmitonRecipes.new(id: params[:id], user: current_user).call
+    if @user_recipe.nil?
+      @user_recipe = ScrappMarmitonRecipes.new(id: params[:id], user: current_user).call
+    end
+  end
+
+  private
+
+  def find_user_recipe
+    @user_recipe = UserRecipe.find(params[:id])
   end
 end
